ae47f1571608d3372b584db5277ac4b0919b9474
yunzhihz <89435007+yunzhihz@users.noreply.github.com>
2021-09-04 19:44:45 +0800

    update
    update

--- thinkphp5/application/index/controller/table.php
+++ thinkphp5/application/index/controller/table.php
@@ -1,15 +1,15 @@
 <?php
 namespace app\index\controller;
-use think\Db;       // 数据库操作类
+use app\commeon\model\Teacher;       // 数据库操作类
 /**
  * 课程管理
  */
-class table
+class TableController
 {
     public function index()
     {
         // 获取教师表中的所有数据
-        $school timetable = Db::name('table')->select();
+        $school timetable = Db::name('Table')->select();
 
         // 查看获取的数据
         var_dump($school timetable);
