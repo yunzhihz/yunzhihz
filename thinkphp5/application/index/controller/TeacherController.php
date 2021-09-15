847e48e101a7a5fbeb1afa1931fb3c7a9ff74150
yunzhihz <89435007+yunzhihz@users.noreply.github.com>
2021-09-15 15:43:45 +0800

    Update TeacherController.php

--- thinkphp5/application/index/controller/TeacherController.php
+++ thinkphp5/application/index/controller/TeacherController.php
@@ -25,5 +25,12 @@ class TeacherController extends controller
 		$teacher['sex'] = '1';
 		$teacher['email'] = 'wangwu@yunzhi.club';
 		var_dump($teacher);
+
+		//引用teacher数据表对应的模型
+		$Teacher = new Teacher();
+		
+		// 向teacher表中插入数据并判断是否插入成功
+		$Teacher->data($teacher)->save();
+		return $teacher['name'] . '成功增加至数据表中'；
 	}
 }
