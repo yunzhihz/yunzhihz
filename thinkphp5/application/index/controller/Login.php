d95b12550f5f9a3e477547bbde90fab190a2ab25
yunzhihz <89435007+yunzhihz@users.noreply.github.com>
2021-11-15 09:34:05 +0800

    update
    update

--- thinkphp5/application/index/controller/Login.php
+++ thinkphp5/application/index/controller/Login.php
@@ -18,7 +18,6 @@ class Login extends Controller
     public function login()
     {
         // 接收post信息
-        
         $postData = Request::instance()->post();
 
         // 验证用户名是否存在
@@ -29,7 +28,7 @@ class Login extends Controller
         if (!is_null($Teacher) && $Teacher->getData('password') === $postData['password']) {
             // 用户名密码正确，将teacherId存session，并跳转至教师管理界面
             session('teacherId', $Teacher->getData('id'));
-            return $this->success('login success', url('classtable/index'));
+            return $this->success('login success', url('controller/Login'));
         } else {
             // 用户名不存在，跳转到登录界面。
             return $this->error('username or password incorrect', url('index'));
