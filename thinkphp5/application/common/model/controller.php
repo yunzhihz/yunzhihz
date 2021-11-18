5f91e49795c15721a43ae4705195eaee414c8cf7
yunzhihz <89435007+yunzhihz@users.noreply.github.com>
2021-11-18 18:54:23 +0800

    1
    1

---
+++ thinkphp5/application/common/model/controller.php
@@ -0,0 +1,42 @@
+namespace app\common\model;
+...
+class Teacher extends Model
+{
+        /**
+     * 用户登录
+     * @param  string $username 用户名
+     * @param  string $password 密码
+     * @return bool   成功返回true，失败返回false。
+     */
+    static public function login($username, $password)
+    {
+        // 验证用户是否存在
+        $map = array('username' => $username);
+        $Teacher = self::get($map);
+        
+        if (!is_null($Teacher)) {
+            // 验证密码是否正确
+            if ($Teacher->checkPassword($password)) {
+                // 登录
+                session('teacherId', $Teacher->getData('id'));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 验证密码是否正确
+     * @param  string $password 密码
+     * @return bool           
+     */
+    public function checkPassword($password)
+    {
+        if ($this->getData('password') === $password)
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
